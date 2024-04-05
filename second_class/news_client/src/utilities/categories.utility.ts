c1557b4d5d1e450496f3f6a717e721949043602c
Pablo Garay <elxebec@gmail.com>
2024-04-05 19:22:36 -0300

    Create utility categories

---
+++ second_class/news_client/src/utilities/categories.utility.ts
@@ -0,0 +1,43 @@
+enum Categories {
+  business = "business",
+  entertainment = "entertainment",
+  general = "general",
+  health = "health",
+  science = "science",
+  sports = "sports",
+  technology = "technology"
+
+}
+
+const categories = [
+  {
+    id: 1,
+    name: Categories.business
+  },
+  {
+    id: 2,
+    name: Categories.entertainment
+  },
+  {
+    id: 3,
+    name: Categories.general
+  },
+  {
+    id: 4,
+    name: Categories.health
+  },
+  {
+    id: 5,
+    name: Categories.science
+  },
+  {
+    id: 6,
+    name: Categories.sports
+  },
+  {
+    id: 7,
+    name: Categories.technology
+  },
+]
+
+export default categories
