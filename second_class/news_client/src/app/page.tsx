0588c6ce88f442236fbec23037706ddd641f14c1
Pablo Garay <elxebec@gmail.com>
2024-04-06 11:38:08 -0300

    Improve index page

--- second_class/news_client/src/app/page.tsx
+++ second_class/news_client/src/app/page.tsx
@@ -1,16 +1,9 @@
-import Image from "next/image";
-import Navbar from "./pages/navbar/Navbar";
-import Footer from "./pages/footer/Footer";
 import LandingPage from "./pages/landing/LandingPages";
 
 export default function Home() {
   return (
-    <>
-      <Navbar />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white-100">
-        <LandingPage />
-      </main>
-      <Footer />
-    </>
+  <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white-100">
+    <LandingPage />
+  </main>
   );
 }
